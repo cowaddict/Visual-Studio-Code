363ede245ed48d8967bc515d433e89feb10ea734
cowaddict <daniele.rufo3@gmail.com>
2024-06-13 06:58:26 -0400

    up

---
+++ prove/prova1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define numquest 3                  //numero domande
+#define nicklenght 100       //numero caratteri nickname consentiti
+
+void presentazione() {   //intro del gioco
+    printf ("\nBenvenuto!!\n");
+    printf("Il gioco sta per cominciare\n");
+    printf ("Rispondete correttamente per collezionare punti.\n");
+    printf ("Ogni risposta esatta garantisce 1 punto!\n");
+    printf ("Buona fortuna!\n");
+    }
+    
+void Menu() { //menu di scelta
+    printf("Vuoi iniziare a giocare?\n");
+    printf("Digita A per cominciare o un tasto a tua scelta per uscire dal gioco.\n"); 
+}
+
+int main() {
+    char scelta;
+    char nickname[nicklenght];
+    int punteggio = 0;
+    
+    char domande[numquest][nicklenght] = {  //array di domande
+        "Quanti stomaci ha una mucca?\nA) 2\nB) 3\nC) 4\n"
+        "Quante ore al giorno mangia una mucca?\nA) 12\nB) 17\nC) 20\n"
+        "Quanti chili, in media, pesa una mucca adulta?\nA) 400\nB)800 C)600"
+    };
+    
+    char risposteUtente[numquest] = {'C', 'B', 'C'};  //array di risposte
+    
+    presentazione();
+    Menu();
+    scanf(" %c", &scelta);
+    
+    if (scelta == 'A' || scelta == 'a') {
+        printf("Inserisci il tuo nickname: ");
+        scanf("%s", nickname);
+        
+        for (int i = 0; i < numquest; i++) {
+            char risposta;
+            printf("%s\n", domande[i]);
+            printf("Inserisci la tua risposta: ");
+            scanf(" %c", &risposta);
+            
+            if (risposta == risposteUtente[i]) {
+                punteggio++;
+            }
+        }
+        
+        printf("Hai totalizzato: %d\n", punteggio);
+        
+    } else {
+        printf("Grazie, torna a giocare quando vuoi!\n");
+    }
+    
+    return 0;
+}
